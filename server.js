6949c744e1efe9788b64d73f1fb230050ba373f9
Ben Gimbel <BenGimbel@Bens-MacBook-Pro.local>
2019-04-17 09:11:33 -0500

    first commit, added server file that I finish after front end

---
+++ server.js
@@ -0,0 +1,3 @@
+// I will create backend with graphQL after I do front end requirements.
+// Usually I would do server setup first, but the requirements are for the front end to use local storage.
+// Once front end is finished, I will implement the backend server with local storage code commented out.
