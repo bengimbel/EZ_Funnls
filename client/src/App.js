97b4a495a167959c9d675b723676cf13966e1b4f
Ben Gimbel <BenGimbel@Bens-MacBook-Pro.local>
2019-04-18 11:40:38 -0500

    added appcompoent code into main component and now app component just renders main

--- client/src/App.js
+++ client/src/App.js
@@ -1,88 +1,12 @@
 import React, { Component } from "react";
-import SearchBar from "./components/Searchbar";
-import ResturantList from "./components/RestaurantList";
-import { fetchLocationByZipCode } from "./api/FetchLocationByZipCode";
-import { fetchResturantList } from "./api/FetchResturantList";
-import "./App.css";
-import ToggleListButton from "./components/ToggleListButton";
+import Main from "./components/Main";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      resturantData: null,
-      cityTitle: null,
-      visitedResturants: [],
-      searchTab: true
-    };
-  }
-
-  executeSearch = zip => {
-    fetchLocationByZipCode(zip)
-      .then(data => {
-        const city = data.results.map(item => item.formatted_address);
-        const lat = data.results.map(item => item.geometry.location.lat);
-        const lng = data.results.map(item => item.geometry.location.lng);
-        fetchResturantList(lat, lng).then(res => {
-          this.setState({
-            cityTitle: city,
-            resturantData: res.results
-          });
-        });
-      })
-      .catch(err => console.log(err, "err"));
-  };
-
-  saveResturantToVisitList = resturant => {
-    const { visitedResturants } = this.state;
-    const newList = [...visitedResturants];
-    if (newList.filter(item => item.id === resturant.id).length > 0) {
-      console.log("already in favorites");
-      return;
-    }
-    newList.unshift(resturant);
-    this.setState({
-      visitedResturants: newList
-    });
-  };
-
-  toggleList = () => {
-    this.setState(prevState => {
-      return {
-        searchTab: !prevState.searchTab
-      };
-    });
-  };
-
   render() {
-    console.log(this.state, "state");
-    const {
-      resturantData,
-      cityTitle,
-      visitedResturants,
-      searchTab
-    } = this.state;
-
     return (
       <div className="App">
         <div className="container">
-          <SearchBar
-            submitZipCode={this.executeSearch}
-            renderSearchList={this.toggleList}
-            isSearchTabActive={searchTab}
-          />
-          <ToggleListButton
-            switchSearchTerm={this.toggleList}
-            searchTabTitle={searchTab}
-          />
-          <ResturantList
-            resturantData={resturantData}
-            cityName={cityTitle}
-            saveResturant={this.saveResturantToVisitList}
-            visitedResturants={visitedResturants}
-            renderSearchList={searchTab}
-          />
+          <Main />
         </div>
       </div>
     );
