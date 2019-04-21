de17ac92f0f9fd4598b95745752caea3bfcd34c5
Ben Gimbel <BenGimbel@Bens-MacBook-Pro.local>
2019-04-21 11:55:56 -0500

    added apolloclient to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
 import Main from "./components/Main";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+//Apollo client setup
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql"
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <Main />
+      <ApolloProvider client={client}>
+        <div className="App">
+          <div className="container">
+            <Main />
+          </div>
         </div>
-      </div>
+      </ApolloProvider>
     );
   }
 }
